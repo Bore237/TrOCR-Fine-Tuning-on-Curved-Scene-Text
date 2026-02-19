631edf832776ca6d86f85b79a095c34a2a3013d2
Bore237 <goudjouborel@gmail.com>
2026-02-19 07:35:20 +0100

    feat(train): Add eraly stoping class that monitoring cer metric

--- src/utils/train_utils.py
+++ src/utils/train_utils.py
@@ -74,3 +74,54 @@ def evaluate_one_epoch(model, processor, val_loader, device, metrics):
             all_cers += cer
 
     return {"wer": all_wers / len(val_loader), "cer": all_cers / len(val_loader) }
+
+class EarlyStopping:
+    def __init__(self, patience=10, min_delta=0.0, mode="min", save_path=None):
+        """
+        Args:
+            patience (int): epochs to wait before stopping
+            min_delta (float): minimum improvement required
+            mode (str): "min" or "max"
+            save_path (str): path to save best model
+        """
+        self.patience = patience
+        self.min_delta = min_delta
+        self.mode = mode
+        self.save_path = save_path
+
+        self.best_score = None
+        self.counter = 0
+        self.early_stop = False
+
+    def __call__(self, metric, model,  epoch, optimizer):
+
+        if self.best_score is None:
+            self.best_score = metric
+            self._save_model(model, epoch, optimizer)
+            return
+
+        improvement = (
+            metric < self.best_score - self.min_delta
+            if self.mode == "min"
+            else metric > self.best_score + self.min_delta
+        )
+
+        if improvement:
+            self.best_score = metric
+            self.counter = 0
+            self._save_model(model, epoch, optimizer)
+        else:
+            self.counter += 1
+            print(f"EarlyStopping counter: {self.counter}/{self.patience}")
+
+            if self.counter >= self.patience:
+                self.early_stop = True
+
+    def _save_model(self, model, epoch, optimizer):
+        if self.save_path and model is not None:
+            checkpoint = {
+                "model_state": model.state_dict(),
+                "optimizer_state": optimizer.state_dict(),
+                "epoch": epoch
+            }
+            torch.save(checkpoint, self.save_path)
