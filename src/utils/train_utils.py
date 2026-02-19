a175ff7b61c89b5ae42fc1d2d2da4dcda4aa96e7
Bore237 <goudjouborel@gmail.com>
2026-02-19 07:32:06 +0100

    feat(training) Add train one epoch
    That inclue auto_cast mode and  return loss and ocr metric(cer and wer)

--- src/utils/train_utils.py
+++ src/utils/train_utils.py
@@ -1,15 +1,35 @@
 import torch
 from tqdm import tqdm
+from torch.amp.autocast_mode import autocast
 
-def train_one_epoch(model, train_dataloader, optimizer, scheduler, device, scaler):
+def compute_ocr_metric(processor, pred_ids, label_ids, metrics):
+    # Load metric
+    wer_metric, cer_metric = metrics
+
+    pred_str = processor.batch_decode(pred_ids, skip_special_tokens=True)
+    # Clone pour éviter modification en place
+    label_ids = label_ids.clone()
+    label_ids[label_ids == -100] = processor.tokenizer.pad_token_id
+    label_str = processor.batch_decode(label_ids, skip_special_tokens=True)
+    
+    cer = cer_metric.compute(predictions=pred_str, references=label_str)
+    wer = wer_metric.compute(predictions=pred_str, references=label_str)
+    return cer, wer
+
+def train_scale_one_epoch(model, processor, train_dataloader, optimizer, scheduler, device, scaler, metrics, config):
     model.train()
+    
+    torch.set_grad_enabled(True)
     total_loss = 0
+    all_wers = 0.0
+    all_cers = 0.0
 
     for batch in tqdm(train_dataloader, desc="Training"):
         pixel_values = batch["pixel_values"].to(device)
         labels = batch["labels"].to(device)
 
-        with torch.amp.autocast(device):
+        # Forward en mixed precision
+        with autocast(device, enabled=config["training"]["mixed_precision"]):
             outputs = model(pixel_values=pixel_values, labels=labels)
             loss = outputs.loss
 
@@ -17,8 +37,20 @@ def train_one_epoch(model, train_dataloader, optimizer, scheduler, device, scale
         scaler.step(optimizer)
         scaler.update()
         optimizer.zero_grad()
-        scheduler.step()
+        
+        if scheduler:
+            scheduler.step()
 
         total_loss += loss.item()
 
-    return total_loss / len(train_dataloader)
+        with torch.no_grad():
+            pred_ids = model.generate(pixel_values=pixel_values)
+            cer, wer = compute_ocr_metric(processor, pred_ids, labels, metrics)
+            all_wers += wer
+            all_cers += cer
+
+    return {
+            "loss": total_loss / len(train_dataloader),
+            "cer": all_cers / len(train_dataloader),
+            "wer": all_wers / len(train_dataloader)
+    }
